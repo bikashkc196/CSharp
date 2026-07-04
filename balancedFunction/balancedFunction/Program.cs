5d0243a6e84ba599112e11f9d7568bc598edfe58
bikashkc196 <bikashkc2057@gmail.com>
2026-07-04 22:44:38 +0545

    lab 1

---
+++ balancedFunction/balancedFunction/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace balancedFunction
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the number of elements in the array: ");
+            int size = Convert.ToInt32(Console.ReadLine());
+
+            int[] userArray = new int[size];
+
+            Console.WriteLine($"Enter {size} integers:");
+            for (int i = 0; i < size; i++)
+            {
+                Console.Write($"Element [{i}]: ");
+                userArray[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            Console.WriteLine("\n--- Checking Array Balance ---");
+
+            if (IsBalanced(userArray))
+            {
+                Console.WriteLine("Result: The array is balanced!");
+            }
+            else
+            {
+                Console.WriteLine("Result: The array is NOT balanced.");
+            }
+
+            Console.WriteLine("\nPress any key to exit...");
+            Console.ReadKey();
+        }
+
+        static bool IsBalanced(int[] arr)
+        {
+            if (arr == null) return false;
+
+            int length = arr.Length;
+
+            if (length % 2 == 0)
+            {
+                return false;
+            }
+
+            int midIndex = length / 2;
+
+            int leftSum = 0;
+            int rightSum = 0;
+
+            for (int i = 0; i < midIndex; i++)
+            {
+                leftSum += arr[i];
+            }
+            for (int i = midIndex + 1; i < length; i++)
+            {
+                rightSum += arr[i];
+            }
+            return leftSum == rightSum;
+        }
+    }
+}
---
+++ jaggedArray/jaggedArray/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace jaggedArray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[][] studentMarks = new int[3][];
+
+            Console.WriteLine("=== Student Marks Entry ===");
+
+            for (int i = 0; i < studentMarks.Length; i++)
+            {
+               
+                Console.Write($"\nEnter the number of subjects for Student {i + 1}: ");
+                int subjectCount = Convert.ToInt32(Console.ReadLine());
+
+                studentMarks[i] = new int[subjectCount];
+
+                
+                for (int j = 0; j < subjectCount; j++)
+                {
+                    Console.Write($"  Enter marks for Subject {j + 1}: ");
+                    studentMarks[i][j] = Convert.ToInt32(Console.ReadLine());
+                }
+            }
+
+
+            for (int i = 0; i < studentMarks.Length; i++)
+            {
+                int total = 0;
+                int subjectCount = studentMarks[i].Length;
+
+                
+                for (int j = 0; j < subjectCount; j++)
+                {
+                    total += studentMarks[i][j];
+                }
+
+                double average = (double)total / subjectCount;
+
+                Console.WriteLine($"--- Student {i + 1} ---");
+                Console.WriteLine($"Total Marks: {total}");
+                Console.WriteLine($"Average Marks: {average:F2}");
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
